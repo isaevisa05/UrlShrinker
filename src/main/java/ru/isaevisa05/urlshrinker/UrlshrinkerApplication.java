4e2521f3b6e13d1377adbdd96da553a32ee1b45c
isaevisa05 <isaevisao55@gmail.com>
2026-05-17 16:55:15 +0300

    Spring initializr

---
+++ src/main/java/ru/isaevisa05/urlshrinker/UrlshrinkerApplication.java
@@ -0,0 +1,13 @@
+package ru.isaevisa05.urlshrinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshrinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshrinkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/isaevisa05/urlshrinker/UrlshrinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isaevisa05.urlshrinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshrinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
